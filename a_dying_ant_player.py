cb78f458dbeb77c17df2c4a10bc287d5a62a6ed0
DustyAcorn <160749468+DustyAcorn@users.noreply.github.com>
2026-02-27 22:13:20 -0500

    added "choose_ant" logic

--- a_dying_ant_player.py
+++ a_dying_ant_player.py
@@ -3,6 +3,8 @@ from random import choice
 from board import Entity, neighbors
 import numpy as np
 import numpy.typing as npt
+import heapq
+from collections import defaultdict
 
 
 AntMove = tuple[tuple[int, int], tuple[int, int]]
@@ -40,9 +42,84 @@ class DyingBot:
     def name(self):
         return "dying_ant"
     
-    def score_land(self, vision) -> set:
+    """
+    FRIENDLY_HILL = 1
+    ENEMY_HILL = 2
+    FRIENDLY_ANT = 3
+    ENEMY_ANT = 4
+    FOOD = 5
+        """
+    
+    def scout_land(self, vision: set[tuple[tuple[int, int], Entity]], ) -> set:
         #dijkstra map of 
         ...
+    
+    def attack_land(self, vision: set[tuple[tuple[int, int], Entity]], ) -> set:
+        #dijkstra map of 
+        ...
+
+    def guard_land(self, vision: set[tuple[tuple[int, int], Entity]], ) -> set:
+        #dijkstra map of 
+        ...
+
+    def choose_role(
+            self, vision: set[tuple[tuple[int, int], Entity]], 
+            radius: int, ant_capacity: int, food_capacity: int, stored_food: int
+            ) -> defaultdict: 
+        """returns some role as an int:
+        Ant Scout = 1
+        Ant Guard = 2
+        Ant Attack = 3
+        """
+
+        my_hills = {coord for coord, kind in vision if kind == Entity.FRIENDLY_HILL}
+        my_ants = {coord for coord, kind in vision if kind == Entity.FRIENDLY_ANT}
+
+        closed_list = set()
+        frontier = []
+
+        stinky = defaultdict(lambda: float(0))
+        near_colony = defaultdict(lambda: float(0))
+
+        ant_type = defaultdict(lambda: float(0))
+
+        for hill in my_hills:        
+            stinky[hill] = -radius
+            near_colony[hill] = 1
+            heapq.heappush(frontier, (stinky[hill], hill))
+
+        while frontier:
+            stink, current_cell = heapq.heappop(frontier)
+            if current_cell in closed_list:
+                continue
+            if stink > 0:
+                break
+            for neighbor in valid_neighbors(*current_cell, self.walls):
+                if neighbor in closed_list:
+                    continue
+                temp_s = stink + 1
+                if temp_s < stinky[neighbor]:
+                    stinky[neighbor] = temp_s
+                    if neighbor not in closed_list:
+                        heapq.heappush(
+                            frontier, (stinky[neighbor], neighbor)
+                        )
+                        near_colony[neighbor] = 1
+                        
+            closed_list.add(current_cell)
+
+        for ant in my_ants:
+            if near_colony[ant]:
+                ant_type[ant] = 2
+            else:
+                if len(my_ants) > ant_capacity and stored_food > food_capacity:
+                    ant_type[ant] = 3
+                else:
+                    ant_type[ant] = 1
+
+        return ant_type
+    #"choose ant function," basically meaning if the ant is farther away, it's a scout
+    #closer, and it's a guard, and if you have more than a certain amount of ants+hills+food all your scouts become attackers
 
     def move_ants(
         self,
@@ -62,8 +139,10 @@ class DyingBot:
             if not valid:
                 claimed_destinations.add(ant)
                 continue
+
             dest = choice(valid)
             #this is where the choice is randomized, by the way
+
             claimed_destinations.add(dest)
             out.add((ant, dest))
         return out
